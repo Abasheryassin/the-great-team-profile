31709eec32939f02a7a2758e7dbec84d0097d74c
abasheryassin <abasheryassin@gmail.com>
2023-01-11 17:18:50 -0600

    final touch ups

--- index.js
+++ index.js
@@ -2,45 +2,45 @@ const inquirer = require("inquirer");
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const fs = require('fs');
 
-const {managerQuestions, internQuestions, engineerQuestions} = require("./src/questions");
-const {htmlStart, htmlEnd} = require("./src/html");
+const { managerQuestions, internQuestions, engineerQuestions } = require("./src/questions");
+const { htmlStart, htmlEnd } = require("./src/html");
 let addedProfiles = ``;
 
 //ask manager to build thier team
 function addEmployee() {
     inquirer
-    .prompt({
-        type: "list",
-        message: "Please choose an employee to add below or finalize team",
-        name: "employee",
-        choices: ["Engineer", "Intern", "Finalize Team"]
-    })
-    .then((response) => {
-        console.log(response);
-        if (response.employee === "Intern"){
-            //write code here
-            addIntern();
-        } else if (response.employee === "Engineer"){
-            //write code here
-            addEngineer();
-        } else {
-            createHTML();
-            return;
-        }
-    })
+        .prompt({
+            type: "list",
+            message: "Please choose an employee to add below or finalize team",
+            name: "employee",
+            choices: ["Engineer", "Intern", "Finalize Team"]
+        })
+        .then((response) => {
+            if (response.employee === "Intern") {
+                //write code here
+                addIntern();
+            } else if (response.employee === "Engineer") {
+                //write code here
+                addEngineer();
+            } else {
+                createHTML();
+                return;
+            }
+        })
 }
 
 //asks for new intern info and saves it
 function addIntern() {
     inquirer
-    .prompt(internQuestions)
-    .then((response) => {
-        let {name, id, email, school} = response;
+        .prompt(internQuestions)
+        .then((response) => {
+            let { name, id, email, school } = response;
 
-        let intern = new Intern(name, id, email, school);
+            let intern = new Intern(name, id, email, school);
 
-        let profile = `<div class="col-4">
+            let profile = `<div class="col-4">
         <div class="p-3 border bg-light">
          <h1>${intern.name}</h1>
          <h2>${intern.getRole()}</h2>
@@ -50,21 +50,21 @@ function addIntern() {
         </div>
        </div>`;
 
-       addedProfiles += profile;
-        addEmployee();
-    });
+            addedProfiles += profile;
+            addEmployee();
+        });
 }
 
 //asks for new engineer info and saves it
 function addEngineer() {
     inquirer
-    .prompt(engineerQuestions)
-    .then((response) => {
-        let {name, id, email, github} = response;
+        .prompt(engineerQuestions)
+        .then((response) => {
+            let { name, id, email, github } = response;
 
-        let engineer = new Engineer(name, id, email, github);
+            let engineer = new Engineer(name, id, email, github);
 
-        let profile = `<div class="col-4">
+            let profile = `<div class="col-4">
         <div class="p-3 border bg-light">
          <h1>${engineer.name}</h1>
          <h2>${engineer.getRole()}</h2>
@@ -74,26 +74,27 @@ function addEngineer() {
         </div>
        </div>`;
 
-       addedProfiles += profile;
-        addEmployee();
-    });
+            addedProfiles += profile;
+            addEmployee();
+        });
 }
 
 function createHTML() {
     let finalHTML = htmlStart + addedProfiles + htmlEnd;
-    
+
+    fs.writeFile("index.html", finalHTML, (err) => err ? console.error(err) : console.log('Success!'));
 }
 
 //Initalizes the program asks for manager information
-function init(){
+function init() {
     inquirer
-    .prompt(managerQuestions)
-    .then((response) => {
-        let {name, id, email, officeNumber} = response;
+        .prompt(managerQuestions)
+        .then((response) => {
+            let { name, id, email, officeNumber } = response;
 
-        let manager = new Manager(name, id, email, officeNumber);
+            let manager = new Manager(name, id, email, officeNumber);
 
-        let profile = `<div class="col-4">
+            let profile = `<div class="col-4">
         <div class="p-3 border bg-light">
          <h1>${manager.name}</h1>
          <h2>${manager.getRole()}</h2>
@@ -103,9 +104,9 @@ function init(){
         </div>
        </div>`;
 
-       addedProfiles += profile;
-        addEmployee();
-    });
+            addedProfiles += profile;
+            addEmployee();
+        });
 }
 
 init();
